fd639a9a99ae91dc6a8a4f610c3ac6db4a32d326
Shruti Agrawal <shruti98932@gmail.com>
2021-06-22 19:15:32 -0400

    Assignment  6ees6ees6ees Completed

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
-};
+
+class HoneyMakerBee extends Bee {
+  constructor(age, job) {
+    super(age);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot += 1;
+  }
+
+  giveHoney() {
+    this.honeyPot -= 1;
+  }
+}
